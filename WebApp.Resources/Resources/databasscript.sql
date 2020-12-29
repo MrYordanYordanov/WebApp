7817b349c4a7d18ad7c242bbad32bd2afcc7c276
MrYordanYordanov <kimi_raikonen_@abv.bg>
2020-12-29 18:38:58 +0200

    commit databasescript

--- WebApp.Resources/Resources/databasscript.sql
+++ WebApp.Resources/Resources/databasscript.sql
@@ -18,11 +18,6 @@ CREATE SCHEMA IF NOT EXISTS `school` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8m
 -- -----------------------------------------------------
 -- Schema schooltest
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema schooltest
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `schooltest` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `school` ;
 
 -- -----------------------------------------------------
@@ -92,81 +87,6 @@ CREATE TABLE IF NOT EXISTS `school`.`students_semesters` (
   CONSTRAINT `students_semesters_ibfk_2`
     FOREIGN KEY (`semester_id`)
     REFERENCES `school`.`semesters` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-USE `schooltest` ;
-
--- -----------------------------------------------------
--- Table `schooltest`.`semesters`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `schooltest`.`semesters` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `startDate` DATE NOT NULL,
-  `endDate` DATE NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `schooltest`.`disciplines`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `schooltest`.`disciplines` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `professorName` VARCHAR(255) NOT NULL,
-  `score` DECIMAL(16,2) NOT NULL,
-  `semester_id` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `semester_id` (`semester_id` ASC),
-  CONSTRAINT `disciplines_ibfk_1`
-    FOREIGN KEY (`semester_id`)
-    REFERENCES `schooltest`.`semesters` (`id`)
-    ON DELETE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 13
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `schooltest`.`students`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `schooltest`.`students` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `surname` VARCHAR(255) NOT NULL,
-  `dob` DATE NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 16
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `schooltest`.`students_semesters`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `schooltest`.`students_semesters` (
-  `student_id` INT NOT NULL,
-  `semester_id` INT NOT NULL,
-  PRIMARY KEY (`student_id`, `semester_id`),
-  INDEX `semester_id` (`semester_id` ASC),
-  CONSTRAINT `students_semesters_ibfk_1`
-    FOREIGN KEY (`student_id`)
-    REFERENCES `schooltest`.`students` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-  CONSTRAINT `students_semesters_ibfk_2`
-    FOREIGN KEY (`semester_id`)
-    REFERENCES `schooltest`.`semesters` (`id`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB
